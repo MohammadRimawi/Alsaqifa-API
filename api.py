96e1277017d351a148def926084325d7b8f9ae49
Mohammad Rimawi <mohrimawiz@gmail.com>
2021-06-27 17:46:04 +0300

    .

--- api.py
+++ api.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, session,make_response
 from flask_assets import Environment, Bundle
 import mysql.connector
 
+from functools import wraps
+
 from utility import *
 
 
@@ -10,6 +12,43 @@ app = Flask(__name__)
 
 # db = mysql.connector.connect(host = "192.168.1.70",user="python-connector",passwd="000000",database = "Alsaqifa")
 db = mysql.connector.connect(host = "127.0.0.1",user="python-connector",passwd="000000",database = "Alsaqifa", auth_plugin='mysql_native_password',autocommit=True)
+
+
+def auth_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        try:
+            auth = request.authorization
+            response = {}
+            if auth:
+                cur = db.cursor(dictionary=True)
+                command = 'SELECT user_id FROM authentication where username = "'+auth.username+'" AND password = "'+auth.password+'"'
+                print(command)
+                cur.execute(command)
+
+                result = cur.fetchall()
+
+                if len(result)!=1:
+                    response["server message"] = "Requires authentication!"
+                    return response,401  #TODO revise the response code!
+                else:
+                    #TODO add role restriction!
+                    response["server message"] = "Hello!"
+                    return f(*args, **kwargs)
+                    pass
+            else:
+                response["server message"] = "Requires authentication!"
+                return response,401  #TODO revise the response code!
+            
+
+        except Exception as e:
+            response["server message"] = 'Server Error!\n"'+str(e)+'"' 
+            cur.close()
+            return response,500
+            pass
+
+    return decorated
+
 
 @app.route('/api')
 def index():
@@ -126,10 +165,23 @@ def post_info(post_name):
         pass
 
 @app.route('/api/add_post',methods=['POST'])
+@auth_required
 def add_post():
-    data = request.get_json()
+
+    #TODO Add authentication
+
+    data = request.get_json()    
     response = {}
 
+    # resp,status = auth_required(request.authorization,"admin")
+
+    # if status == 200:
+    #     response["authorization"] = "Authorized"
+    #     print("Authorized!")
+    # else:
+    #     print("Not Authorized!")
+    #     return resp,status
+    print("test test")
     try:
         cur = db.cursor(dictionary=True)
         command = 'SELECT post_id FROM posts WHERE title = "'+data["title"]+'"'
@@ -217,7 +269,64 @@ def playlist(playlist_name):
 
 
 
+@app.route('/api/add_tag',methods=['POST'])
+@auth_required
+def add_tag():
+    
+    data = request.get_json()
+    response = {}
+    try:
+        cur = db.cursor(dictionary=True)
+        command = 'SELECT tag_id FROM tags WHERE tag_name = "'+data["tag_name"]+'"' 
+        cur.execute(command)
+        result = cur.fetchall()
+        if len(result) == 0:
+            command = 'INSERT INTO `tags`(`tag_name`) VALUE ("'+data["tag_name"]+'")'
+            cur.execute(command)
+            response["server message"] = 'Added successfully!'
 
+            return response,201
+        else:
+            response["server message"] = 'Was not added!,Tag name conflict with tag_id = "'+str(result[0]["tag_id"])+'"'
+            cur.close()
+            return response,409
+        pass
+    except Exception as e:
+        response["server message"] = 'Server Error!\n"'+str(e)+'"' 
+        cur.close()
+        return response,500
+        pass
+
+@app.route('/api/tag_post',methods=['POST'])
+@auth_required
+def tag_post():
+    data = request.get_json()
+    response = {}
+
+    try:
+        cur = db.cursor(dictionary=True)
+        command = 'SELECT * FROM posts_tags WHERE post_id = "'+str(data["post_id"])+'" AND tag_id = "'+str(data["tag_id"])+'"'
+        cur.execute(command)
+
+        result = cur.fetchall()
+        print(result)
+        if(len(result)!=0):
+            response["server message"] = 'Was not added!,Post with post_id = "'+str(result[0]["post_id"])+'" already has tag_id = "'+str(result[0]["tag_id"])+'"'
+            cur.close()
+            return response,409
+        
+        else:
+            command = 'INSERT INTO `posts_tags`(`tag_id`,`post_id`) VALUES ('+str(data["tag_id"])+','+str(data["post_id"])+')'
+            cur.execute(command)
+            response["server message"] = 'Added successfully!'
+
+            return response,201
+        pass
+    except Exception as e:
+        response["server message"] = 'Server Error!\n"'+str(e)+'"' 
+        cur.close()
+        return response,500
+        pass
 
 
 if __name__ == '__main__':
